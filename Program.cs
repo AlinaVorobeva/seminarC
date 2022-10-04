94893c460d9c0b8a149ef80ba33f2455277d249a
Alina <nevorobeva0101@mail.ru>
2022-10-04 23:26:23 +0300

    задача 4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// задача №4 : напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел .
+
+int a, b, c ;
+a = new Random().Next(100) ;
+b = new Random().Next(100) ;
+c = new Random().Next(100) ;
+int max = a ;
+Console.Write($"{a} {b} {c}") ;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write($" Максимальное число = {max}") ;
+
